4a9b870a9b9be000a5d6b361345edd5c5f4cd947
Het Shah <het203007@gmail.com>
2026-06-28 12:11:59 +0530

    Thursday: camera node, cv_bridge integration, live camera feed

---
+++ src/trucon_camera/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'trucon_camera'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='piys',
+    maintainer_email='het203007@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'camera_node = trucon_camera.camera_node:main'
+        ],
+    },
+)
---
+++ src/trucon_camera/trucon_camera/camera_node.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+
+class CameraNode(Node):
+    def __init__(self):
+        super().__init__('camera_node')
+        self.bridge = CvBridge()
+        self.subscription = self.create_subscription(
+            Image,
+            '/head_camera/image_raw',
+            self.image_callback,
+            10)
+        self.get_logger().info('Camera node has been started, waiting for image messages...')
+
+    def image_callback(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        cv2.imshow('Trucon Camera Feed', cv_image)
+        cv2.waitKey(1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
